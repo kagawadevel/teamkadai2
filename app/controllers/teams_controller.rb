379417596f8da740810fcfdee9c7dc6d122a6f00
kakegawa <cmuwni@gmail.com>
2020-01-29 07:36:53 +0900

    チーム編集の権限機能を修正

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,7 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_team, only: %i[show edit update destroy]
-  before_action :owner_action, only: %i[show edit]
+  before_action :owner_action, only: %i[edit update]
 
   def index
     @teams = Team.all
