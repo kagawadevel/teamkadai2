a6bfb7f17631a1c07dac3770ee44666af5a2eedb
kakegawa <cmuwni@gmail.com>
2020-02-02 12:57:09 +0900

    ()アジェンダの削除機能とその後のメール機能を追加

--- app/mailers/assign_mailer.rb
+++ app/mailers/assign_mailer.rb
@@ -6,4 +6,11 @@ class AssignMailer < ApplicationMailer
     @password = password
     mail to: @email, subject: I18n.t('views.messages.complete_registration')
   end
+
+  def delete_agenda(user_id, agenda)
+    @agenda = agenda
+    @user = User.find_by(id: user_id)
+    @email = @user.email
+    mail to: @email, subject: 'アジェンダを削除しました'
+  end
 end
